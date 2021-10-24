6cf534537ccd85108a94583519cb7381da5e942d
XIANGMAN ZHAO <xiangmaz@andrew.cmu.edu>
2021-10-24 14:15:45 -0400

    hw5

--- HW-5/Q3/Q3.py
+++ HW-5/Q3/Q3.py
@@ -1,17 +1,44 @@
-def check(x,y):
+def check1(x, y):
     edit = 0
-    if (len(x)>=len(y)):
-        for i in x:
-            if (i not in y):
-                edit = edit + 1
-    else:
-        for j in y:
-            if (j not in x):
-                edit = edit + 1
+    for i in x:
+        if (i not in y):
+            edit = edit + 1    
     if (edit <= 1):
         return True
     else:
         return False
+        
+
+def check2(x,y):
+    edit = 0
+    for j in y:
+        if (j not in x):
+            edit = edit + 1
+
+    if (edit <= 1):
+        return True
+    else:
+        return False
+
+def check3(x,y):
+    edit = 0
+    for i in range(len(x)):
+        if (x[i]!=y[i]):
+            edit = edit + 1
+    if (edit <= 1):
+        return True
+    else:
+        return False
+
+def check(x,y):
+    if (abs(len(x)-len(y))>1):
+        return False
+    if (len(x)>len(y)):
+        return check1(x,y)
+    elif (len(y)>len(x)):
+        return check2(x,y)
+    else:
+        return check3(x,y)
     
 
 a = "pale"
